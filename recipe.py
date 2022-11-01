2ab8d060611fbcd47dffdbe536d1d1b32e7e6587
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-01 21:56:41 +0530

    first

---
+++ recipe.py
@@ -0,0 +1,72 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host = 'localhost' , user = 'root' , password = '' , database = 'recipedb')
+mycursor = mydb.cursor()
+while True:
+
+    print("select an option from the menu")
+
+    print("1 add recipe")
+
+    print("2 view all recipe")  
+
+    print("3 search a recipe")
+
+    print("4 update the recipe")    
+
+    print("5 delete a recipe")
+
+    print("6 exit")
+
+   
+
+    choice = int(input('enter an option:'))
+
+    if(choice==1):
+
+        print('recipe enter selected')
+        
+        
+        
+
+        name = input('enter the name')
+
+        category = input('enter the category')
+
+        taste = input('enter the taste ')
+
+        price = input('enter the price')
+        
+        
+        
+        sql = 'INSERT INTO `recipe`( `name`, `category`, `taste`, `price`) VALUES (%s,%s,%s,%s)'
+
+        
+
+        data = (name,category,taste,price)
+
+        mycursor.execute(sql , data)
+
+        mydb.commit()
+        
+        
+
+    elif(choice==2):
+
+        print('view recipe')
+
+    elif(choice==3):
+
+        print('search recipe')
+
+    elif(choice==4):
+
+        print('update recipe')
+
+    elif(choice==5):
+
+        print('delete recipe')
+
+    elif(choice==6):
+
+        break
